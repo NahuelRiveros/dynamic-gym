b4f08a1307c1ce6f680df206fc014a97a9ea9498
Riveros Edgardo Nahuel <70718987+NahuelRiveros@users.noreply.github.com>
2026-03-03 10:00:03 -0300

    indexcss

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./app/query_client.js";
 import { router } from "./app/router.jsx";
 import { AuthProvider } from "./auth/auth_context.jsx"; // Login Futuro
-import "./index.css";
+import "../src/index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
